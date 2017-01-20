809ca2dc44784510ab047acdcfc63f14822d72d4
Jesse LaRusso <jesse@animoto.com>
2017-01-20 13:16:47 -0500

    Remove struct scoping on sauce interactor

--- interactors/make_sauce.go
+++ interactors/make_sauce.go
@@ -4,17 +4,15 @@ import (
 	"github.com/jlarusso/gonads/maybe"
 )
 
-type SauceInteractor struct{}
-
 // This particular interactor requires this kind of map as the params
-func (si SauceInteractor) Act(params interface{}) maybe.Monad {
+func MakeSauce(params interface{}) maybe.Monad {
 	return maybe.Maybe(params).
-		Bind(si.Prepare).
-		Bind(si.Cook).
-		Bind(si.Taste)
+		Bind(Prepare).
+		Bind(Cook).
+		Bind(Taste)
 }
 
-func (_ SauceInteractor) Prepare(params interface{}, m maybe.Monad) maybe.Monad {
+func Prepare(params interface{}, m maybe.Monad) maybe.Monad {
 	// NOTE: I'd really like to have some kind of sanitization step where the params coming into
 	// the interactor.  The type assertion below would happen there (instead of at the top of each
 	// method).  There'd be some kind of context struct specific to this package is passed from
@@ -29,7 +27,7 @@ func (_ SauceInteractor) Prepare(params interface{}, m maybe.Monad) maybe.Monad 
 	}
 }
 
-func (_ SauceInteractor) Cook(params interface{}, m maybe.Monad) maybe.Monad {
+func Cook(params interface{}, m maybe.Monad) maybe.Monad {
 	p := params.(map[string]int)
 
 	if p["heat"] < 80 {
@@ -39,7 +37,7 @@ func (_ SauceInteractor) Cook(params interface{}, m maybe.Monad) maybe.Monad {
 	}
 }
 
-func (_ SauceInteractor) Taste(params interface{}, m maybe.Monad) maybe.Monad {
+func Taste(params interface{}, m maybe.Monad) maybe.Monad {
 	p := params.(map[string]int)
 
 	if p["salt"] < 2 {
--- main.go
+++ main.go
@@ -6,12 +6,13 @@ import (
 )
 
 func main() {
+
 	p1 := make(map[string]int)
 	p1["tomatoes"] = 1
 	p1["heat"] = 100
 	p1["salt"] = 2
 
-	result1 := interactors.SauceInteractor{}.Act(p1)
+	result1 := interactors.MakeSauce(p1)
 	fmt.Println(result1) // => Failure(Not enough tomatoes)
 
 	p2 := make(map[string]int)
@@ -19,7 +20,7 @@ func main() {
 	p2["heat"] = 20
 	p2["salt"] = 25
 
-	result2 := interactors.SauceInteractor{}.Act(p2)
+	result2 := interactors.MakeSauce(p2)
 	fmt.Println(result2) // => Failure(Turn up the heat)
 
 	p3 := make(map[string]int)
@@ -27,6 +28,6 @@ func main() {
 	p3["heat"] = 100
 	p3["salt"] = 2
 
-	result3 := interactors.SauceInteractor{}.Act(p3)
+	result3 := interactors.MakeSauce(p3)
 	fmt.Println(result3) // => Success(30)
 }
